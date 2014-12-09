041205ba6ddf8ab70570e40b3744af5f44bfcbdc
stephen <s.knox@ucl.ac.uk>
2014-12-09 12:49:21 +0000

    update to demo2. still not working

--- Tutorial/Demo models/Demo 2/Demo2.py
+++ Tutorial/Demo models/Demo 2/Demo2.py
@@ -9,7 +9,7 @@ import coopr.environ
 from coopr.opt import SolverFactory
 
 # Declaring the model
-model = AbstractModel()
+model = AbstractModel(name="demo2 model")
 
 # Declaring model indexes using sets
 model.nodes = Set()
@@ -18,7 +18,6 @@ model.demand_nodes = Set()
 model.nonstorage_nodes = Set()
 model.storage_nodes = Set()
 model.time_step = Set()
-model.t = Set()
 
 # Declaring model parameters
 model.inflow = Param(model.nodes, model.time_step)
@@ -31,97 +30,90 @@ model.flow_upper_bound = Param(model.links, model.time_step)
 model.storage_lower_bound = Param(model.storage_nodes, model.time_step)
 model.storage_upper_bound = Param(model.storage_nodes, model.time_step)
 
+def current_flow_capacity_constraint(instance, node, node2):
+    """
+        Defining the flow lower and upper bound
+    """
+    return (instance.flow_lower_bound[node, node2, instance.t],
+            instance.flow_upper_bound[node, node2, instance.t])
+
+def current_storage_capacity_constraint(instance, storage_nodes):
+    """
+        Defining the storage lower and upper bound
+    """
+    return (instance.storage_lower_bound[storage_nodes, instance.t],
+            instance.storage_upper_bound[storage_nodes, instance.t])
+
+def objective_function(instance):
+    return summation(instance.cost, instance.X)
+
+def mass_balance(instance, nonstorage_nodes):
+    """
+        Mass balance for non-storage nodes:
+    """
+
+    # inflow
+    term1 = instance.inflow[nonstorage_nodes, instance.t]
+    term2 = sum([instance.X[node_in, nonstorage_nodes, instance.t]*instance.flow_multiplier[node_in, nonstorage_nodes, instance.t] for node_in in instance.nodes if (node_in, nonstorage_nodes) in instance.links])
+    
+    # outflow
+    term3 = instance.consumption_coefficient[nonstorage_nodes, instance.t] * sum([instance.X[node_in, nonstorage_nodes, instance.t]*instance.flow_multiplier[node_in, nonstorage_nodes, instance.t] for node_in in instance.nodes if (node_in, nonstorage_nodes) in instance.links])
+    term4 = sum([instance.X[nonstorage_nodes, node_out] for node_out in instance.nodes if (nonstorage_nodes, node_out) in instance.links])
+
+    # inflow - outflow = 0:
+    return (term1 + term2) - (term3 + term4) == 0
+
+def storage_mass_balance(instance, storage_nodes):
+    """
+        Mass balance for storage nodes
+    """
+    # inflow
+    term1 = instance.inflow[storage_nodes, instance.t]
+    term2 = sum([instance.X[node_in, storage_nodes, instance.t]*instance.flow_multiplier[node_in, storage_nodes, instance.t] for node_in in instance.nodes if (node_in, storage_nodes) in instance.links])
+
+    # outflow
+    term3 = sum([instance.X[storage_nodes, node_out, instance.t] for node_out in instance.nodes if (storage_nodes, node_out) in instance.links])
+
+    # storage
+    term4 = instance.new_init_S[storage_nodes, instance.t]
+    term5 = instance.S[storage_nodes, instance.t]
+
+    # inflow - outflow = 0:
+    return (term1 + term2 + term4) - (term3 + term5) == 0
+
 ##======================== running the model in a loop for each time step
 def run():
     opt = SolverFactory("glpk")
     instance = model.create("Demo2.dat")
     for t in instance.time_step:
-        #model.t = Set([t])
-        for link in model.links:
-            model.c[link] = Param(model.links, model.t, initialize=instance.cost)
-            model.flow_mult[link] = Param(model.links, model.t, initialize=instance.flow_multiplier)
-            model.min_flow[link] = Param(model.links, model.t, initialize=instance.flow_lower_bound)
-            model.max_flow[link] = Param(model.links, model.t, initialize=instance.flow_upper_bound)
-
-
-        for node in model.nodes:
-            model.Inflow[node] = Param(model.nodes, model.t, initialize=instance.inflow)
-            model.cc[node] = Param(model.nodes, model.t, initialize=instance.consumption_coefficient)
-
-        for s_node in model.storage_nodes:
-            model.min_storage[s_node] = Param(model.storage_nodes, model.t, initialize=instance.storage_lower_bound)
-            model.max_storage[s_node] = Param(model.storage_nodes, model.t, initialize=instance.storage_upper_bound)
-
-##======================================== Declaring Variables (X and S)
-
-        # Defining the flow lower and upper bound
-        def current_flow_capacity_constraint(model, node, node2):
-            return (model.min_flow[node, node2], model.max_flow[node, node2])
-
-        # Defining the storage lower and upper bound
-        def current_storage_capacity_constraint(model, storage_nodes):
-            return (model.min_storage[storage_nodes], model.max_storage[storage_nodes])
+        instance.t = t
 
         # Declaring decision variable X
-        model.X = Var(model.links, domain=NonNegativeReals, bounds=current_flow_capacity_constraint)
+        instance.X = Var(instance.links, domain=NonNegativeReals, bounds=current_flow_capacity_constraint)
 
         # Declaring state variable S
-        model.S = Var(model.storage_nodes, domain=NonNegativeReals, bounds=current_storage_capacity_constraint)
+        instance.S = Var(instance.storage_nodes, domain=NonNegativeReals, bounds=current_storage_capacity_constraint)
 
-##======================================== Declaring the objective function (Z)
+        # Declaring state variable S
+        instance.c = Var(instance.nodes, domain=NonNegativeReals, bounds=current_cost)
 
-        def objective_function(model):
-            return summation(model.X, model.c)
 
-        model.Z = Objective(model.links, rule=objective_function, sense=minimize)
+        instance.Z = Objective(rule=objective_function, sense=minimize)
 
-##======================================== Declaring constraints
 
-##------- Mass balance for non-storage nodes:
+        instance.mass_balance_const = Constraint(instance.nonstorage_nodes, rule=mass_balance)
 
-        def mass_balance(model, nonstorage_nodes):
-
-    # inflow
-            term1 = model.Inflow[nonstorage_nodes]
-            term2 = sum([model.X[node_in, nonstorage_nodes]*model.flow_mult[node_in, nonstorage_nodes] for node_in in model.nodes if (node_in, nonstorage_nodes) in model.links])
-
-    # outflow
-            term3 = model.cc[nonstorage_nodes] * sum([model.X[node_in, nonstorage_nodes]*model.flow_mult[node_in, nonstorage_nodes] for node_in in model.nodes if (node_in, nonstorage_nodes) in model.links])
-            term4 = sum([model.X[nonstorage_nodes, node_out] for node_out in model.nodes if (nonstorage_nodes, node_out) in model.links])
-
-    # inflow - outflow = 0:
-            return (term1 + term2) - (term3 + term4) == 0
-
-        model.mass_balance_const = Constraint(model.nonstorage_nodes, rule=mass_balance)
-
-##------- Mass balance for storage nodes:
-        def storage_mass_balance(model, storage_nodes):
-
-    # inflow
-            term1 = model.inflow[storage_nodes]
-            term2 = sum([model.X[node_in, storage_nodes]*model.flow_multiplier[node_in, storage_nodes] for node_in in model.nodes if (node_in, storage_nodes) in model.links])
-
-    # outflow
-            term3 = sum([model.X[storage_nodes, node_out] for node_out in model.nodes if (storage_nodes, node_out) in model.links])
-
-    # storage
-            term4 = model.new_init_S[storage_nodes]
-            term5 = model.S[storage_nodes]
-
-    # inflow - outflow = 0:
-            return (term1 + term2 + term4) - (term3 + term5) == 0
-
-        model.storage_mass_balance_const = Constraint(model.storage_nodes, rule=storage_mass_balance)
+        instance.storage_mass_balance_const = Constraint(instance.storage_nodes, rule=storage_mass_balance)
 
         print ('========================================================')
         res = opt.solve(instance)
-        model.pprint()
+        instance.pprint()
         #instance.load(res)
         #res.write()
         #block.add_new_init_S = instance.S
         #block.del_S()
         #new_init_S = instance.S
-        #model.S = Param(initialize=new_init_S)
+        #instance.S = Param(initialize=new_init_S)
 
 if __name__ == '__main__':
     run()
