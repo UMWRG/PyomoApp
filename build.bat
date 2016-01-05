68a2228eaa578c86423b126d14f69808bd02c5eb
knoxsp <knoxsp@gmail.com>
2016-01-05 09:18:26 +0000

    Update the build script to use the PyomoApp folder instsead of PyomoPlugin

--- build.bat
+++ build.bat
@@ -1,22 +1,22 @@
 cd Auto
 pyinstaller -y --upx-dir=../../upx PyomoAutoRun.spec
-xcopy dist\PyomoAutoRun\* ..\PyomoPlugin\plugins\PyomoFiles\ /Y /E
-copy plugin.xml ..\PyomoPlugin\plugins\PyomoAutoRun\plugin.xml /Y
-copy pyomo_auto_16.png ..\PyomoPlugin\plugins\PyomoAutoRun\pyomo_auto_16.png /Y
-copy pyomo_auto_32.png ..\PyomoPlugin\plugins\PyomoAutoRun\pyomo_auto_32.png /Y
-copy p*.bat ..\PyomoPlugin\plugins\PyomoAutoRun\p*.bat /Y
+xcopy dist\PyomoAutoRun\* ..\PyomoApp\plugins\PyomoFiles\ /Y /E
+copy plugin.xml ..\PyomoApp\plugins\PyomoAutoRun\plugin.xml /Y
+copy pyomo_auto_16.png ..\PyomoApp\plugins\PyomoAutoRun\pyomo_auto_16.png /Y
+copy pyomo_auto_32.png ..\PyomoApp\plugins\PyomoAutoRun\pyomo_auto_32.png /Y
+copy p*.bat ..\PyomoApp\plugins\PyomoAutoRun\p*.bat /Y
 cd ../Export
 pyinstaller -y --upx-dir=../../upx PyomoExport.spec
-xcopy dist\PyomoExport\* ..\PyomoPlugin\plugins\PyomoFiles\ /Y /E
-copy plugin.xml ..\PyomoPlugin\plugins\PyomoExport\plugin.xml /Y
-copy pyomo_export_16.png ..\PyomoPlugin\plugins\PyomoExport\pyomo_export_16.png /Y
-copy pyomo_export_32.png ..\PyomoPlugin\plugins\PyomoExport\pyomo_export_32.png /Y
-copy p*.bat ..\PyomoPlugin\plugins\PyomoExport\p*.bat /Y
+xcopy dist\PyomoExport\* ..\PyomoApp\plugins\PyomoFiles\ /Y /E
+copy plugin.xml ..\PyomoApp\plugins\PyomoExport\plugin.xml /Y
+copy pyomo_export_16.png ..\PyomoApp\plugins\PyomoExport\pyomo_export_16.png /Y
+copy pyomo_export_32.png ..\PyomoApp\plugins\PyomoExport\pyomo_export_32.png /Y
+copy p*.bat ..\PyomoApp\plugins\PyomoExport\p*.bat /Y
 cd ../RunImport
 pyinstaller -y --upx-dir=../../upx PyomoRunImport.spec
-xcopy dist\PyomoRunImport\* ..\PyomoPlugin\plugins\PyomoFiles\ /Y /E
-copy plugin.xml ..\PyomoPlugin\plugins\PyomoRunImport\plugin.xml /Y
-copy pyomo_import_16.png ..\PyomoPlugin\plugins\PyomoRunImport\pyomo_import_16.png /Y
-copy pyomo_import_32.png ..\PyomoPlugin\plugins\PyomoRunImport\pyomo_import_32.png /Y
-copy p*.bat ..\PyomoPlugin\plugins\PyomoRunImport\p*.bat /Y
+xcopy dist\PyomoRunImport\* ..\PyomoApp\plugins\PyomoFiles\ /Y /E
+copy plugin.xml ..\PyomoApp\plugins\PyomoRunImport\plugin.xml /Y
+copy pyomo_import_16.png ..\PyomoApp\plugins\PyomoRunImport\pyomo_import_16.png /Y
+copy pyomo_import_32.png ..\PyomoApp\plugins\PyomoRunImport\pyomo_import_32.png /Y
+copy p*.bat ..\PyomoApp\plugins\PyomoRunImport\p*.bat /Y
 pause
